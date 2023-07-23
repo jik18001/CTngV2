1b28048e0a78bf92a197e235ad96b43491ea4f20
jie <jie.kong@uconn.edu>
2023-07-23 17:05:15 -0400

    more experiment

--- tests/Experiments/exp4/Test1.go
+++ tests/Experiments/exp4/Test1.go
@@ -13,27 +13,27 @@ import (
 )
 
 func StartCA(CID string) {
-	path_prefix := "../ca_testconfig/" + CID
+	path_prefix := "ca_testconfig/" + CID
 	path_1 := path_prefix + "/CA_public_config.json"
 	path_2 := path_prefix + "/CA_private_config.json"
 	path_3 := path_prefix + "/CA_crypto_config.json"
 	ctx_ca := CA.InitializeCAContext(path_1, path_2, path_3)
-	ctx_ca.Maxlatency = 500
+	ctx_ca.Maxlatency = 10
 	CA.StartCA(ctx_ca)
 }
 
 func StartLogger(LID string) {
-	path_prefix := "../logger_testconfig/" + LID
+	path_prefix := "logger_testconfig/" + LID
 	path_1 := path_prefix + "/Logger_public_config.json"
 	path_2 := path_prefix + "/Logger_private_config.json"
 	path_3 := path_prefix + "/Logger_crypto_config.json"
 	ctx_logger := Logger.InitializeLoggerContext(path_1, path_2, path_3)
-	ctx_logger.Maxlatency = 500
+	ctx_logger.Maxlatency = 10
 	Logger.StartLogger(ctx_logger)
 }
 
 func StartMonitor(MID string) {
-	path_prefix := "../monitor_testconfig/" + MID
+	path_prefix := "monitor_testconfig/" + MID
 	path_1 := path_prefix + "/Monitor_public_config.json"
 	path_2 := path_prefix + "/Monitor_private_config.json"
 	path_3 := path_prefix + "/Monitor_crypto_config.json"
@@ -48,12 +48,12 @@ func StartMonitor(MID string) {
 	time.Sleep(60 * time.Second)
 	ctx_monitor.Period_Offset = util.GetCurrentPeriod()
 	ctx_monitor.Maxdrift_miliseconds = 1000
-	ctx_monitor.Clockdrift_miliseconds = rand.Intn(1000)
+	ctx_monitor.Clockdrift_miliseconds = rand.Intn(5)
 	monitor.StartMonitorServer(ctx_monitor)
 }
 
 func StartGossiper(GID string) {
-	path_prefix := "../gossiper_testconfig/" + GID
+	path_prefix := "gossiper_testconfig/" + GID
 	path_1 := path_prefix + "/Gossiper_public_config.json"
 	path_2 := path_prefix + "/Gossiper_private_config.json"
 	path_3 := path_prefix + "/Gossiper_crypto_config.json"
@@ -64,7 +64,7 @@ func StartGossiper(GID string) {
 	ctx_gossiper.Total_Logger = 1
 	ctx_gossiper.Total_CA = 1
 	ctx_gossiper.Min_latency = 0
-	ctx_gossiper.Max_latency = 250
+	ctx_gossiper.Max_latency = 5
 	// create the storage directory if not exist
 	util.CreateDir(ctx_gossiper.StorageDirectory)
 	// create the storage file if not exist
